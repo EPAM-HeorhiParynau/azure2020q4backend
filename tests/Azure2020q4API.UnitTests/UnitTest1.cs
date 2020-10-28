685573ea6dbdfa0c80e49dd1cebec390f75fe45c
Heorhi Parynau <heorhi.parynau@onsolve.com>
2020-10-28 17:59:35 +0300

    Add test prject

---
+++ tests/Azure2020q4API.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Azure2020q4API.UnitTests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			true.Should().BeTrue();
+		}
+	}
+}
