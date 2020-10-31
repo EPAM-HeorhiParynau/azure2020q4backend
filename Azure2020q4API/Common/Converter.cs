0e517fbb677e15c7bea2937abf04ab5560ca3c65
Heorhi Parynau <heorhi.parynau@onsolve.com>
2020-10-31 19:27:55 +0300

    add unit tests

---
+++ Azure2020q4API/Common/Converter.cs
@@ -0,0 +1,10 @@
+﻿namespace Azure2020q4API.Common
+{
+	public class Converter
+	{
+		public string Convert(int input)
+		{
+			return input.ToString();
+		}
+	}
+}
---
+++ tests/Azure2020q4API.UnitTests/Common/ConverterTests.cs
@@ -0,0 +1,26 @@
+﻿using Azure2020q4API.Common;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Azure2020q4API.UnitTests.Common
+{
+	[TestClass]
+	public class ConverterTests
+	{
+		private Converter _converter;
+
+		[TestInitialize]
+		public void Init()
+		{
+			_converter = new Converter();
+		}
+
+		[TestMethod]
+		public void ConverterHappyPath()
+		{
+			var outcome = _converter.Convert(6);
+
+			outcome.Should().Be("6");
+		}
+	}
+}
